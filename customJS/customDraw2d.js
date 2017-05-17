b34a156345774cc46f7d81edc42af8511414980a
Leon-624 <leonxu624@gmail.com>
2017-05-17 15:00:33 -0600

    Implement layer shifting on drop if overlapping with another layer: Create myDraw2d.Util, myDraw2d.shapeDesc.LayerRectangle, myDraw2d.shapeDesc.NodeCircle. Modify onDrop() in CustomCanvas: if not overlaps with other LayerRectangles or NodeCircles, or can avoid by shifting, create layer. if cannot avoid overlap with other LayerRectangles or NodeCircles, no action. Important method: decideLayerCenterPos() in myDraw2d.Util. Logics: (a) Return original center position if it doesn't overlap with another LayerRectangle or any NodeCircle. (b) Return null if it doesn't overlap with another LayerRectangle, but overlaps with any NodeCircle. (c) Return re-calculated center position if it overlaps with another LayerRectangle, but can be shifted to avoid overlap with any LayerRectangle or NodeCircle. (only do x-axis shifting since layers are usually arranged horizontally.) (d) Return null if after shifting it still overlaps with any LayerRectangle or NodeCircle.

--- customJS/customDraw2d.js
+++ customJS/customDraw2d.js
@@ -1,50 +1,67 @@
+/**
+ * @class myDraw2d
+ * global namespace declarations
+ */
+var myDraw2d = 
+{
+    shapeDesc: {
+    }
+};
+
+/**
+ * @class draw2d.CustomCanvas
+ * @author  Liyan Xu
+ * @extends draw2d.policy.EditPolicy
+ */
 draw2d.CustomCanvas = draw2d.Canvas.extend({
 
 	NAME: "draw2d.CustomCanvas",
 
+	/**
+     * @constructor 
+     */
 	init: function(id){
 		this._super(id);
 		this.layerRecs = [];	//array of LayerRectangles
+		this.nodeCirs = [];		//array of NodeCircles
+		this.util = new myDraw2d.Util();
 	},
 
-	//override onDrop for dropping elements from outside div
+	/**
+     * @override
+     * override onDrop for dropping elements from outside div
+     */
 	onDrop: function(droppedDomNode, x, y){
 		var shape = $(droppedDomNode).data('shape');
 		if(shape === 'rectangle')
 		{
-			//if (x, y) is inside another LayerRectangle, no action
-			var ifInLayer = false, layerLen = this.layerRecs.length;
-			for(var i = 0; i < layerLen; ++i)
+			var layerDesc = new myDraw2d.shapeDesc.LayerRectangle(
+				x, y, this.util.DEFAULT_WIDTH_RECTANGLE, this.util.DEFAULT_HEIGHT_RECTANGLE);
+			var centerPos = this.util.decideLayerCenterPos(layerDesc, this.layerRecs, this.nodeCirs);
+			//if not overlaps with other LayerRectangles or NodeCircles, or can avoid by shifting, create layer
+			if(centerPos)
 			{
-				if(this.layerRecs[i].hitTest(x, y))
-				{
-					ifInLayer = true;
-					break;
-				}
-			}
-			//(x, y) is inside another layer
-			if(ifInLayer)
-			{
-				console.log('Cannot add layer on top of another layer');
-			}
-			//(x, y) is not inside another layer
-			else
-			{
-				//let (x, y) be the center of the image
-				var imgObj = new draw2d.shape.basic.LayerRectangle();
-				var cmd = new draw2d.command.CommandAdd(this, imgObj, x-imgObj.getWidth()/2, y-imgObj.getHeight()/2);
+				var imgObj = new draw2d.shape.basic.LayerRectangle(this.util);
+				var cmd = new draw2d.command.CommandAdd(this, imgObj,
+					centerPos.getX()-imgObj.getWidth()/2, centerPos.getY()-imgObj.getHeight()/2);
 				this.getCommandStack().execute(cmd);
 				//add this layer to layerRecs array
 				this.layerRecs.push(imgObj);
 			}
+			//if cannot avoid overlap with other LayerRectangles or NodeCircles, no action
+			else
+			{
+				console.log("Cannot add overlapped layer");
+			}
 		}
-			
 		else if(shape === 'circle')
 		{
-			var imgObj = new draw2d.shape.basic.NodeCircle();
 			//let (x, y) be the center of the image
+			var imgObj = new draw2d.shape.basic.NodeCircle(this.util);
 			var cmd = new draw2d.command.CommandAdd(this, imgObj, x-imgObj.getWidth()/2, y-imgObj.getHeight()/2);
 			this.getCommandStack().execute(cmd);
+			//add this node to nodeCirs array
+			this.nodeCirs.push(imgObj);
 		}
 		else
 		{
@@ -55,17 +72,24 @@ draw2d.CustomCanvas = draw2d.Canvas.extend({
 });
 
 
-
+/**
+ * @class draw2d.policy.canvas.CustomFadeoutDecorationPolicy
+ * @author  Liyan Xu
+ * @extends draw2d.policy.canvas.FadeoutDecorationPolicy
+ */
 draw2d.policy.canvas.CustomFadeoutDecorationPolicy = draw2d.policy.canvas.FadeoutDecorationPolicy.extend({
 
 	NAME: "draw2d.policy.canvas.CustomFadeoutDecorationPolicy",
 
-	//this.DEFAULT_FADEOUT_DURATION controls the time interval to start fading out
-	//this.DEFAULT_ALPHA_DECREMENT controls the time interval of actual fading out
+	/**
+     * @constructor 
+     */
 	init: function()
     {
         this._super();
         this.alpha = 1.0;
+        //this.DEFAULT_FADEOUT_DURATION controls the time interval to start fading out
+		//this.DEFAULT_ALPHA_DECREMENT controls the time interval of actual fading out
         //override this.DEFAULT_FADEOUT_DURATION to a lower value
         this.DEFAULT_FADEOUT_DURATION = 15;
         this.alphaDec = this.DEFAULT_ALPHA_DECREMENT;
@@ -73,7 +97,10 @@ draw2d.policy.canvas.CustomFadeoutDecorationPolicy = draw2d.policy.canvas.Fadeou
         this.portDragging = false;
     },
 
-    //need ports always shown when dragging images or ports
+    /**
+     * @override
+     * need ports always shown when dragging images or ports
+     */
     onMouseDrag: function(canvas, dx, dy, dx2, dy2, shiftKey, ctrlKey)
     {
         //if(this.portDragging === false){
@@ -88,11 +115,18 @@ draw2d.policy.canvas.CustomFadeoutDecorationPolicy = draw2d.policy.canvas.Fadeou
 
 
 
-
+/**
+ * @class draw2d.policy.connection.CustomConnectionCreatePolicy
+ * @author  Liyan Xu
+ * @extends draw2d.policy.connection.ComposedConnectionCreatePolicy
+ */
 draw2d.policy.connection.CustomConnectionCreatePolicy = draw2d.policy.connection.ComposedConnectionCreatePolicy.extend({
 
 	NAME: "draw2d.policy.connection.CustomConnectionCreatePolicy",
 
+	/**
+     * @constructor 
+     */
 	init: function(){
 		this._super([
 			new draw2d.policy.connection.DragConnectionCreatePolicy({
@@ -124,16 +158,26 @@ draw2d.policy.connection.CustomConnectionCreatePolicy = draw2d.policy.connection
 });
 
 
-
+/**
+ * @class draw2d.policy.canvas.CustomSingleSelectionPolicy
+ * @author  Liyan Xu
+ * @extends draw2d.policy.canvas.SingleSelectionPolicy
+ */
 draw2d.policy.canvas.CustomSingleSelectionPolicy = draw2d.policy.canvas.SingleSelectionPolicy.extend({
 
 	NAME: "draw2d.policy.canvas.CustomSingleSelectionPolicy",
 
+	/**
+     * @constructor
+     */
 	init: function(){
 		this._super();
 	},
 
-	//override onClick to bring the clicked figure to the front
+	/**
+     * @override
+     * override onClick to bring the clicked figure to the front
+     */
 	onClick: function(figureClicked, mouseX, mouseY, shiftKey, ctrlKey){
 		if(figureClicked != null && figureClicked.name == 'LayerRectangle')
 		{
@@ -144,16 +188,23 @@ draw2d.policy.canvas.CustomSingleSelectionPolicy = draw2d.policy.canvas.SingleSe
 });
 
 
-
+/**
+ * @class draw2d.shape.basic.LayerRectangle
+ * @author  Liyan Xu
+ * @extends draw2d.shape.basic.Rectangle
+ */
 draw2d.shape.basic.LayerRectangle = draw2d.shape.basic.Rectangle.extend({
 
 	NAME: "draw2d.shape.basic.LayerRectangle",
 
-	init: function(){
-		var me = this;
+	/**
+     * @constructor
+     * @param {myDraw2d.Util}
+     */
+	init: function(util){
 		this._super({
-			width: 60,
-			height: 200,
+			width: util.DEFAULT_WIDTH_RECTANGLE,
+			height: util.DEFAULT_HEIGHT_RECTANGLE,
 			bgColor: '#FFFFC7',
 			radius: 10,
 			glow: true
@@ -162,20 +213,41 @@ draw2d.shape.basic.LayerRectangle = draw2d.shape.basic.Rectangle.extend({
 		this.childNodes = [];	//array of NodeCircles inside this layer
 	},
 
+	/**
+     * @override
+     * override onDragEnd to bring the layer to the back
+     */
 	onDragEnd: function(){
 		this._super();
 		this.toBack();
+	},
+
+	/**
+     * @method
+     * @returns {draw2d.geo.Point} the center point of LayerRectangle
+     */
+	getCenterPos: function(){
+		return new draw2d.geo.Point(this.x + this.width/2, this.y + this.height/2);
 	}
 
 });
 
-
+/**
+ * @class draw2d.shape.basic.NodeCircle
+ * @author  Liyan Xu
+ * @extends draw2d.shape.basic.Circle
+ */
 draw2d.shape.basic.NodeCircle = draw2d.shape.basic.Circle.extend({
 
 	NAME: "draw2d.shape.basic.NodeCircle",
 
-	init: function(){
+	/**
+     * @constructor
+     * @param {myDraw2d.Util}
+     */
+	init: function(util){
 		this._super({
+			radius: util.DEFAULT_RADIUS_CIRCLE,
 			stroke: 1,
 			color: '#000000',
 			bgColor: '#00A4D1',
@@ -183,10 +255,311 @@ draw2d.shape.basic.NodeCircle = draw2d.shape.basic.Circle.extend({
 		});
 		this.name = 'NodeCircle'; 
 		this.parentLayer = null;	//parent LayerRectangle
+	},
+
+	/**
+     * @method
+     * @returns {draw2d.geo.Point} the center point of NodeCircle
+     */
+	getCenterPos: function(){
+		return new draw2d.geo.Point(this.x + this.radius, this.y + this.radius);
 	}
 
 });
 
+
+/**
+ * @class myDraw2d.Util
+ * @author  Liyan Xu
+ */
+myDraw2d.Util = Class.extend({
+    
+	NAME: "myDraw2d.Util",
+
+	/**
+     * @constructor
+     */
+	init: function(){
+		this.DEFAULT_WIDTH_RECTANGLE = 60;
+		this.DEFAULT_HEIGHT_RECTANGLE = 200;
+		this.DEFAULT_RADIUS_CIRCLE = 20;
+	},
+
+	/**
+     * @method
+     * @param {myDraw2d.shapeDesc.LayerRectangle} layer
+     * @param {array} layerRecs, nodeCirs properties of CustomCanvas
+     * @returns {draw2d.geo.Point} or {null}
+     (a) Return original center position if it doesn't overlap with another LayerRectangle
+	 or any NodeCircle.
+	 (b) Return null if it doesn't overlap with another LayerRectangle, but overlaps with
+	 any NodeCircle.
+	 (c) Return re-calculated center position if it overlaps with another LayerRectangle,
+	 but can be shifted to avoid overlap with any LayerRectangle or NodeCircle.
+	 (only do x-axis shifting since layers are usually arranged horizontally.)
+	 (d) Return null if after shifting it still overlaps with any LayerRectangle or NodeCircle.
+     */
+	decideLayerCenterPos: function(layer, layerRecs, nodeCirs){
+		var firstOvlpLayerDesc = null,
+			layerLen = layerRecs.length,
+			nodeLen = nodeCirs.length;
+		for(var i = 0; i < layerLen; ++i)
+		{
+			var currLayer = layerRecs[i];
+			var currLayerDesc = new myDraw2d.shapeDesc.LayerRectangle(
+				currLayer.getX() + currLayer.getWidth()/2,
+				currLayer.getY() + currLayer.getHeight()/2,
+				currLayer.getWidth(),
+				currLayer.getHeight());
+			if(this.ifLayersOverlap(layer, currLayerDesc))
+			{
+				firstOvlpLayerDesc = currLayerDesc;
+				break;
+			}
+		}
+		//there is no overlapped LayerRectangle
+		if(firstOvlpLayerDesc === null)
+		{
+			var ifOverlapNode = false;
+			for(var i = 0; i < nodeLen; ++i)
+			{
+				var currNode = nodeCirs[i];
+				var currNodeDesc = new myDraw2d.shapeDesc.NodeCircle(
+					currNode.getX() + currNode.getRadius(),
+					currNode.getY() + currNode.getRadius(),
+					currNode.getRadius());
+				if(this.ifLayerOverlapNode(layer, currNodeDesc))
+				{
+					ifOverlapNode = true;
+					break;
+				}
+			}
+			//there exists overlapped NodeCircle, return null
+			if(ifOverlapNode)
+			{
+				console.log("no overlapped LayerRectangle but overlapped NodeCircle");
+				return null;
+			}
+			//there is no overlapped NodeCircle, return original center position
+			else
+			{
+				console.log("no overlapped LayerRectangle or NodeCircle");
+				return new draw2d.geo.Point(layer.centerX, layer.centerY);
+			}
+		}
+		//there exists overlapped LayerRectangle
+		else
+		{
+			var offset = null;
+			if(layer.centerX > firstOvlpLayerDesc.centerX)
+				offset = layer.width/2 + firstOvlpLayerDesc.width/2 - (layer.centerX - firstOvlpLayerDesc.centerX) + 5;
+			else
+				offset = (firstOvlpLayerDesc.centerX - layer.centerX) - (layer.width/2 + firstOvlpLayerDesc.width/2) - 5;
+			console.log("center pos before applying offset: " + layer.centerX + ", " + layer.centerY);
+			//apply offset to shift
+			layer.centerX += offset;
+			console.log("center pos after applying offset: " + layer.centerX + ", " + layer.centerY);
+			firstOvlpLayerDesc = null;
+			for(var i = 0; i < layerLen; ++i)
+			{
+				var currLayer = layerRecs[i];
+				var currLayerDesc = new myDraw2d.shapeDesc.LayerRectangle(
+					currLayer.getX() + currLayer.getWidth()/2,
+					currLayer.getY() + currLayer.getHeight()/2,
+					currLayer.getWidth(),
+					currLayer.getHeight());
+				if(this.ifLayersOverlap(layer, currLayerDesc))
+				{
+					firstOvlpLayerDesc = currLayerDesc;
+					break;
+				}
+			}
+			//if still overlaps with other LayerRectangle, return null
+			if(firstOvlpLayerDesc)
+			{
+				console.log("after shifting still overlaps with LayerRectangles");
+				return null;
+			}
+			var ifOverlapNode = false;
+			for(var i = 0; i < nodeLen; ++i)
+			{
+				var currNode = nodeCirs[i];
+				var currNodeDesc = new myDraw2d.shapeDesc.NodeCircle(
+					currNode.getX() + currNode.getRadius(),
+					currNode.getY() + currNode.getRadius(),
+					currNode.getRadius());
+				if(this.ifLayerOverlapNode(layer, currNodeDesc))
+				{
+					ifOverlapNode = true;
+					break;
+				}
+			}
+			//there exists overlapped NodeCircle, return null
+			if(ifOverlapNode)
+			{
+				console.log("after shifting no overlapped LayerRectangles but overlaps with NodeCircle");
+				return null;
+			}
+			//there is no overlapped NodeCircle, return shifted center position
+			else
+			{
+				console.log("after shifting no overlapped LayerRectangles or NodeCircle");
+				return new draw2d.geo.Point(layer.centerX, layer.centerY);
+			}
+		}
+	},
+
+	/**
+     * @method
+     * @param {myDraw2d.shapeDesc.LayerRectangle} l1, l2
+     * @returns {boolean} true if two LayerRectangles overlap
+     */
+	ifLayersOverlap: function(l1, l2){
+		if((Math.abs(l2.centerX - l1.centerX) > (l1.width/2 + l2.width/2)) ||
+			(Math.abs(l2.centerY - l1.centerY) > (l1.height/2 + l2.height/2)))
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	},
+
+	/**
+     * @method
+     * @param {myDraw2d.shapeDesc.LayerRectangle} layer
+     * @param {myDraw2d.shapeDesc.NodeCircle} node
+     * @returns {boolean} true if LayerRectangle overlaps any NodeCircle
+     */
+	ifLayerOverlapNode: function(layer, node){
+		//first, if center is inside LayerRectangle, return true
+		if(layer.ifPointInside(node.centerX, node.centerY))
+			return true;
+		//next, if two centers have distance > width/2 + radius on x-axis,
+		//or distance > height/2 + radius on y-axis, return false
+		if((Math.abs(layer.centerX - node.centerX) > (layer.width/2 + node.radius))
+			|| ((Math.abs(layer.centerY - node.centerY)) > (layer.height/2 + node.radius)))
+		{
+			return false;
+		}
+		//next, if center of node is around the four corners, and distance between
+		//center and vertex of layer > radius, return false
+		var vertexX = null, vertexY = null;
+		if(node.centerX > (layer.centerX + layer.width/2))
+		{
+			if(node.centerY > (layer.centerY + layer.height/2))
+			{
+				vertexX = layer.centerX + layer.width/2;
+				vertexY = layer.centerY + layer.height/2;
+			}
+			else if(node.centerY < (layer.centerY - layer.height/2))
+			{
+				vertexX = layer.centerX + layer.width/2;
+				vertexY = layer.centerY - layer.height/2;
+			}
+		}
+		else if(node.centerX < (layer.centerX - layer.width/2))
+		{
+			if(node.centerY > (layer.centerY + layer.height/2))
+			{
+				vertexX = layer.centerX - layer.width/2;
+				vertexY = layer.centerY + layer.height/2;
+			}
+			else if(node.centerY < (layer.centerY - layer.height/2))
+			{
+				vertexX = layer.centerX - layer.width/2;
+				vertexY = layer.centerY - layer.height/2;
+			}
+		}
+		if(vertexX)
+		{
+			if(this.getDistance(vertexX, vertexY, node.centerX, node.centerY) > node.radius)
+			{
+				return false;
+			}
+		}
+		//finally, for the rest of scenarios, return true
+		return true;
+	},
+
+	/**
+     * @method
+     * @param {Number} x1, y1 position of first point
+     * @param {number} x2, y2 position of second point
+     * @returns {Number} the distance between two points
+     */
+	getDistance: function(x1, y1, x2, y2){
+		var tmp1 = (x1 - x2),
+			tmp2 = (y1 - y2);
+		return Math.sqrt(tmp1*tmp1 + tmp2*tmp2);
+	}
+
+});
+
+
+
+/**
+ * @class myDraw2d.shapeDesc.LayerRectangle
+ * This class describes position and size of LayerRectangle.
+ * @author  Liyan Xu
+ */
+myDraw2d.shapeDesc.LayerRectangle = Class.extend({
+    
+	NAME: "myDraw2d.shapeDesc.LayerRectangle",
+
+	/**
+     * @constructor 
+     */
+	init: function(centerX, centerY, width, height){
+		this.centerX = centerX;
+		this.centerY = centerY;
+		this.width = width;
+		this.height = height;
+	},
+
+	/**
+     * @method
+     * @param {Number} x
+     * @param {number} y
+     * @returns {boolean} true if the point is inside this LayerRectangle
+     */
+	ifPointInside: function(x, y){
+		var halfWidth = this.width / 2,
+			halfHeight = this.height / 2;
+		if((x >= (this.centerX - halfWidth) && x <= (this.centerX + halfWidth))
+			&& (y >= (this.centerY - halfHeight) && y <= (this.centerY + halfHeight)))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+});
+
+
+/**
+ * @class myDraw2d.shapeDesc.NodeCircle
+ * This class describes position and size of NodeCircle.
+ * @author  Liyan Xu
+ */
+myDraw2d.shapeDesc.NodeCircle = Class.extend({
+    
+	NAME: "myDraw2d.shapeDesc.NodeCircle",
+
+	/**
+     * @constructor 
+     */
+	init: function(centerX, centerY, radius){
+		this.centerX = centerX;
+		this.centerY = centerY;
+		this.radius = radius;
+	}
+
+});
 
 
 
